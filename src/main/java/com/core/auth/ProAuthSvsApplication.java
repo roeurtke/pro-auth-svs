159c6971f7e51c0e43926f58911d6f55df14efa8
roeurtke <kesey.roeurth@gmail.com>
2025-12-17 22:39:39 +0700

    first commit

---
+++ src/main/java/com/core/auth/ProAuthSvsApplication.java
@@ -0,0 +1,13 @@
+package com.core.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProAuthSvsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProAuthSvsApplication.class, args);
+	}
+
+}
